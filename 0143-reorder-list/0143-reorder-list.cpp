27e1394cc3cfc11981ef9b859ff6faf73fc56f9f
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-03-23 09:19:10 +0530

    Time: 28 ms (30.27%), Space: 21.3 MB (69.02%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* findmiddle(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* reverse(ListNode* head){
+        ListNode* curr=head;
+        ListNode* prev=nullptr;
+        ListNode* nxt=head;
+        while(curr!=NULL){
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+        }
+        return prev;
+    }
+    void reorderList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return;
+        
+        ListNode* middle = findmiddle(head);
+        ListNode* secondHalf = middle->next;
+        middle->next = nullptr; 
+        secondHalf = reverse(secondHalf);
+        
+        ListNode* first = head;
+        while (secondHalf != nullptr) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = secondHalf->next;
+            first->next = secondHalf;
+            secondHalf->next = temp1;
+            first = temp1;
+            secondHalf = temp2;
+        }
+    }
+};
