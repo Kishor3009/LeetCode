9c7165588d95845632cc6e95a65049a0af217dee
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-03-27 09:40:26 +0530

    Time: 18 ms (100.00%), Space: 63.9 MB (13.98%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         if(k<=1) return 0;
         int prod=1;
         int res=0;
