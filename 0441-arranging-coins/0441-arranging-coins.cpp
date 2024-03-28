f205bb95ae5ef921fb3b2845e2f79831936a51f0
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-03-28 15:00:56 +0530

    Time: 9 ms (19.33%), Space: 7.5 MB (86.69%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         if(n==0) return 0;
         int k=0;
         int tot=1;
