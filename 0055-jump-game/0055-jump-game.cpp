e5a111fe9e64ce38417c03c7d61f3ad6505214cf
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-03-28 15:57:18 +0530

    Time: 30 ms (98.14%), Space: 51 MB (24.21%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-    int n = nums.size();
-    int lastPos = n - 1; // The last position we need to reach
-    
-    // Start from the last second position and update lastPos if we can reach it
-    for (int i = n - 2; i >= 0; i--) {
-        if (i + nums[i] >= lastPos) {
-            lastPos = i;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int n = nums.size();
+        int lastPos = n - 1; 
+        for (int i = n - 2; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
         }
+        return lastPos == 0;
     }
-    
-    // If lastPos is the first position, it means we can reach the last index
-    return lastPos == 0;
-}
 
 };
