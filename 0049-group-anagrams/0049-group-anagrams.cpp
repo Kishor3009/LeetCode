a117411acab0f3184012ee7cb08955111f9cb678
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-03-31 13:50:24 +0530

    Time: 21 ms (87.50%), Space: 23.7 MB (69.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         unordered_map<string,vector<std::string>> anagramGroups;
 
         for (string& word : strs) {
