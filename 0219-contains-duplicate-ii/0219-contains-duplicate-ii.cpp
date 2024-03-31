3548c63c9fd1b05483eee62ce6317c9682f4d738
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-03-31 12:09:42 +0530

    Time: 90 ms (99.10%), Space: 76 MB (86.29%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         unordered_set<int> seen;
         for (int i = 0; i < nums.size(); ++i) {
             if (seen.find(nums[i]) != seen.end()) {
