b849952b70d9b1bce51ff19663ddf680194d052c
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-03-31 13:31:52 +0530

    Time: 38 ms (99.46%), Space: 71 MB (66.40%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -4,15 +4,12 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         int n=arr.size();
-        int maxi=INT_MIN;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                maxi=max(maxi,arr[j]);
-            }
+        int maxi=-1,tmp;
+        for(int i=n-1;i>=0;i--){
+            tmp=arr[i];
             arr[i]=maxi;
-            maxi=INT_MIN;
+            maxi=max(maxi,tmp);
         }
-        arr[n-1]=-1;
         return arr;
     }
 };
