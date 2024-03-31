26ffad5ff233d57e7981155cca348d3d53a917e3
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-03-31 12:00:07 +0530

    Time: 66 ms (98.79%), Space: 72.9 MB (71.82%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(std::vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         unordered_map<int, int> mpp;
         for (int num : nums) {
             if (mpp[num] >= 1) {
