7b16bffdfd0cbaa973c6632dd80bd1bd93a2c551
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-03-31 13:36:52 +0530

    Time: 3 ms (47.49%), Space: 7.5 MB (69.44%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int pos=0;
         for(int i=0;i<t.size();i++){
             if(t[i]==s[pos]) pos++;
