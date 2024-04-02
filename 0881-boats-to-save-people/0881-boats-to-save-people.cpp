b031793d6439492ac3b374e3cc1448cc0e118999
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-04-02 11:28:27 +0530

    Time: 52 ms (84.84%), Space: 45.5 MB (44.08%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& p, int limit) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int n=p.size();
         int high=n-1;
         int l=0;
