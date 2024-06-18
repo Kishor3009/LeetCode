d36e79763c3816c376a0f92d90c7daf90fdbb06f
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-06-18 17:24:05 +0530

    Time: 256 ms (60.53%), Space: 89.5 MB (52.63%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        vector<pair<int, int>> mp;
+
+        for (int num : nums) {
+            string numStr = to_string(num);
+            string str;
+
+            for (char digit : numStr) {
+                int i = digit - '0'; 
+                str += to_string(mapping[i]);  
+            }
+
+            int rp = stoi(str);  
+            mp.push_back({rp, num});
+        }
+
+        stable_sort(mp.begin(), mp.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+            return a.first < b.first;
+        });
+        vector<int> ans;
+        for (auto it : mp) {
+            ans.push_back(it.second);
+        }
+
+    return ans;
+    }
+};
