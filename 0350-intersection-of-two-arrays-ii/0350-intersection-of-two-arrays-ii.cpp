e94d350e93706fccb0bbf829d81227b2c2f28bb3
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-07-02 09:31:21 +0530

    Time: 4 ms (59.42%), Space: 12.6 MB (75.54%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> intersection;
+
+        set_intersection(nums1.begin(), nums1.end(),
+                          nums2.begin(), nums2.end(),
+                          back_inserter(intersection));
+    
+        return intersection;
+    }
+};
