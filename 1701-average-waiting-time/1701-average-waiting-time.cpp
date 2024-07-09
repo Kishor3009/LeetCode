1909480580b3e313ad5bcb416770579ba6f64c99
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-07-09 09:16:11 +0530

    Time: 150 ms (57.65%), Space: 76.4 MB (95.59%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& cust) {
+        double s=0,tot=0;
+        double d=0;
+        double n=cust.size();
+        tot=cust[0][1];
+        s=cust[0][0]+cust[0][1];
+        for(int i=1;i<n;i++){
+            if(s>=cust[i][0]){
+                s+=cust[i][1];
+            }
+            else{
+                s=cust[i][0]+cust[i][1];
+            }
+            tot+=s-cust[i][0];
+        }
+        d=tot/n;
+        return d;
+    }
+};
