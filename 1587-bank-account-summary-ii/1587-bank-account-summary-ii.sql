12418c7f37f821d38eb2c3ac5110c0a28f2d7750
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-07-21 14:55:20 +0530

    Time: 958 ms (32.06%), Space: 0B (100.00%) - LeetHub

---
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select u.name as NAME, sum(t.amount) as BALANCE FROM Users u, Transactions t
+where u.account = t.account
+group by u.account
+having sum(t.amount)>10000;
