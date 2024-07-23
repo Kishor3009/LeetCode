26d6a78cdf39375b70b31e222a2279b0012c3d73
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-07-23 12:01:16 +0530

    Time: 30 ms (97.55%), Space: 51 MB (36.71%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -3,14 +3,21 @@ public:
     bool canJump(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        int n = nums.size();
-        int lastPos = n - 1; 
-        for (int i = n - 2; i >= 0; i--) {
-            if (i + nums[i] >= lastPos) {
-                lastPos = i;
-            }
+        // int n = nums.size();
+        // int lastPos = n - 1; 
+        // for (int i = n - 2; i >= 0; i--) {
+        //     if (i + nums[i] >= lastPos) {
+        //         lastPos = i;
+        //     }
+        // }
+        // return lastPos == 0;
+        
+        int maxind=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>maxind) return false;
+            maxind=max(maxind,nums[i]+i);
         }
-        return lastPos == 0;
+        return true;
     }
 
 };
